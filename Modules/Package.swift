30100b8f1557f6e7670c9b26bd8ae2281eee88b6
Anderson Lentz <anderlentz@gmail.com>
2022-04-07 21:21:50 -0300

    Add ComposableArchitecure module dependency

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -21,12 +21,17 @@ let package = Package(
             url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
             from: "1.9.0"
           ),
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            from: "0.9.0"
+        )
     ],
     targets: [
         .target(
             name: "HeroesFeature",
             dependencies: [
-                "CoreUI"
+                "CoreUI",
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
             ]
         ),
         .testTarget(
