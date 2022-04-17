5dc1d2f41b78530e73840408e0db7545bb7c22cd
Anderson Lentz <anderlentz@gmail.com>
2022-04-17 17:27:47 -0300

    Add app reducer navigation tests

---
+++ MarvelHeroesTests/AppReducerTests.swift
@@ -0,0 +1,79 @@
+import Combine
+import ComposableArchitecture
+import Foundation
+import XCTest
+@testable import MarvelHeroes
+@testable import HeroesFeature
+@testable import Feature_HeroDetails
+
+class AppReducerTest: XCTestCase {
+    
+    func test_navigateToHeroDetails_shouldChangeHeroDetailsState() {
+        let anyData: Data = .init()
+        let heroCellData: HeroCellData = .init(id: 0, name: "mock", thumbnail: anyData)
+        let environment: AppEnvironment = .init(
+            heroesEnvironment: .failing,
+            heroDetailsEnvironment: .failing
+        )
+
+        let store = TestStore(
+            initialState: AppState(),
+            reducer: appReducer,
+            environment: environment
+        )
+
+        store.send(.heroes(.navigateToHeroDetails(heroCellData: heroCellData))) {
+            $0.heroDetailsState = .init(
+                title: heroCellData.name,
+                description: heroCellData.description,
+                imageData: heroCellData.thumbnail,
+                characterId: heroCellData.id
+            )
+        }
+    }
+    
+    func test_setNavigation_whenIsNotActive_shouldSetToNillHeroDetailsState() {
+        
+        let initialHeroDetailState: HeroDetailsState = .init(
+            title: "Name",
+            description: "Description",
+            imageData: .init(),
+            characterId: 123
+        )
+        
+        let environment: AppEnvironment = .init(
+            heroesEnvironment: .failing,
+            heroDetailsEnvironment: .failing
+        )
+
+        let store = TestStore(
+            initialState: AppState(heroState: .init(), heroDetailsState: initialHeroDetailState),
+            reducer: appReducer,
+            environment: environment
+        )
+
+        store.send(.heroes(.setNavigation(isActive: false))) {
+            $0.heroDetailsState = nil
+        }
+    }
+}
+
+extension HeroesEnvironment {
+    static let failing: Self = .init(marvelCharactersLoader: { _ in
+        XCTFail("\(Self.self) is unimplemented.")
+        return Empty(completeImmediately: true).eraseToAnyPublisher()
+    }, loadThumbnail: { _ in
+        XCTFail("\(Self.self) is unimplemented.")
+        return Empty(completeImmediately: true).eraseToAnyPublisher()
+    }, loadMarvelCharacters: { _ in
+        XCTFail("\(Self.self) is unimplemented.")
+        return Empty(completeImmediately: true).eraseToAnyPublisher()
+    }, mainQueue: .failing)
+}
+
+extension HeroDetailsEnvironment {
+    static let failing: Self = .init(loadCharacterDetails: { _ in
+        XCTFail("\(Self.self) is unimplemented.")
+        return Empty(completeImmediately: true).eraseToAnyPublisher()
+    }, mainQueue: .failing)
+}
